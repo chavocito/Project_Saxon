f8482c38a2a6c84e02d5f54c547805e7005cfafb
Francis Bota <francbota94@gmail.com>
2023-04-14 14:09:49 +0000

    add config files

--- nuxt-fullstack-tutorial/nuxt.config.ts
+++ nuxt-fullstack-tutorial/nuxt.config.ts
@@ -1,4 +1,17 @@
+import { defineNuxtConfig } from 'nuxt';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.js',
+        exposeConfig: false,
+        config: {},
+        injecPosition: 0,
+        viewer: true
+    },
+    colorMode: {
+        classSuffix: ''
+    }
 })
